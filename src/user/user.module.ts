10b3d4f52c7abf10134a39ba652463429f8d8aa0
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2024-05-26 12:05:01 +0300

    add: AdminDatabaseProvider to providers of user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TenantProvider } from '../database/providers/tenant.provider';
 import { Auth0Service } from '../auth0/auth0.service';
+import { AdminDatabaseProvider } from 'src/admin/providers/database.providers';
 
 @Module({
   imports: [],
-  providers: [UserService, TenantProvider, Auth0Service],
+  providers: [UserService, TenantProvider, Auth0Service, AdminDatabaseProvider[0]],
   controllers: [UserController],
 })
-export class UserModule {}
+export class UserModule { }
